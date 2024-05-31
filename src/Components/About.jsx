8c2deac7aacc6bdbdfa960c59aab3dc4844708b8
roopammazumder <roopam10830@yahoo.com>
2024-05-31 20:59:58 +0530

    add all changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/src/images/ocean-8665369_1920.jpg";
+import image from "../images/ocean-8665369_1920.jpg";
 
 const imageAltText = "Wonderful background with sea and ocean";
 
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/src/images/trekking-7593276_1920.jpg";
+import image from "../images/trekking-7593276_1920.jpg";
 
 const imageAltText = "Wonderful picture of trekking and all";
 
